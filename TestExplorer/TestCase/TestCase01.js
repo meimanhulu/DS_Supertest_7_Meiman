9c8feda80e8f7abba4fc9cbda6c78ee71c8f2b15
meimanhulu <94421861+meimanhulu@users.noreply.github.com>
2024-04-15 13:10:54 +0700

    clear

---
+++ TestExplorer/TestCase/TestCase01.js
@@ -0,0 +1,46 @@
+//const { postMethod, putMethod, getMethod, patchMethod, deleteMethod, postPayloadMethod } = require("../APIServer/APIObjects");
+const { deleteMethod } = require("../APIServer/deleteTest");
+const { getMethod } = require("../APIServer/getTest");
+const { patchMethod } = require("../APIServer/patchTest");
+const { postPayloadMethod } = require("../APIServer/postpayloadTest");
+const { postMethod } = require("../APIServer/postTest");
+const { putMethod } = require("../APIServer/putTest");
+
+
+
+//Test Suites
+
+// Test Case POST using payload method
+describe("Testing API restful", function () {
+let id;
+
+  it("Test POST from Function", async function () {
+    id = await postPayloadMethod();
+  });
+
+  // Test case POST
+  it("Test POST from Function", async function () {
+     id = await postMethod();
+  });
+
+  // Test case PUT
+  it("Test PUT from Function", async function () {
+    await putMethod(id);
+  });
+  
+  // Test case GET
+  it("Test GET from Function", async function () {
+    await getMethod(id);
+  });
+
+  // Test case PATCH
+  it("Test PATCH from Function", async function () {
+    await patchMethod(id);
+  });
+
+  // Test case DELETE
+  it("Test DELETE from Function", async function () {
+    await deleteMethod(id);
+  });
+
+});
